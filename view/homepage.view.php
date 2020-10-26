b181caad68db95650f96ba36f5d76a2436958067
gabriel rossier <rossier.gabriel218@gmail.com>
2020-10-26 11:55:57 +0100

    homepage

--- view/homepage.view.php
+++ view/homepage.view.php
@@ -1,5 +1,21 @@
 <?php ob_start(); ?>
-<h1 class="text-center p-5">Home</h1>
+
+<div class="Welcome">
+    <h1>
+        Bienvenue sur esportTeam.ch
+    </h1>
+    <img src="view/images/trophy.png"/>
+    <p>
+        Ce site a pour but de faciliter le recrutement et la recherche d'équipe d'esport en suisse.
+        Chaque personne pourra ainsi créer une équipe et rechercher des joueurs.
+    </p>
+</div>
+<div class="siteStatus">
+
+    <h1>Le site est en construction.</h1>
+
+</div>
+
 <?php
 $content = ob_get_clean();
 require_once "layout.view.php";
