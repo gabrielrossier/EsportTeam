b1e95f4386ed9c98f3859484f426f609a7d936ad
gabriel rossier <rossier.gabriel218@gmail.com>
2020-10-26 11:59:17 +0100

    Add

---
+++ view/layout.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>EsportTeam</title>
+    <link rel="stylesheet" href="view/styles/custom.css">
+</head>
+
+<body>
+    <div class="sideNav">
+        <a href="" title="Page des teams"><img src="view/images/team.png" /></a>
+        <a href="" title="Page des tournois"><img src="view/images/tournament.png" /></a>
+        <a href="" title="Page des succès"><img src="view/images/trophy.png" /></a>
+        <div class="ProfileIcone"><a href="" title="Profile"><img src="view/images/player.png"/></a></div>
+    </div>
+    <div class="main">
+        <?= $content ?>
+    </div>
+</body>
